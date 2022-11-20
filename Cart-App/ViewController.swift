d26ad1ae7259f17bb91f88d047f0df5f228492dc
Hussein <hussein.elsherief@gmail.com>
2022-11-20 09:03:04 +0200

    Initial Commit

---
+++ Cart-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cart-App
+//
+//  Created by Hussein Anwar on 20/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cart-AppUITests/Cart_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Cart_AppUITestsLaunchTests.swift
+//  Cart-AppUITests
+//
+//  Created by Hussein Anwar on 20/11/2022.
+//
+
+import XCTest
+
+class Cart_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
